255693a8bdeb1ff16a8d0a1b8dac6df59e02eec0
Justin Martin <jjmardev@gmail.com>
2021-06-30 11:20:16 -0400

    Fix linting errors

--- dice_maiden_ui/command.py
+++ dice_maiden_ui/command.py
@@ -53,11 +53,13 @@ def calculate_dice_roll(num_dice, num_dice_sides, base_modifier,
     adv_dis = calculate_advantage_disadvantage(advantage, disadvantage)
     modifier = calculate_modifier(base_modifier, extra_modifier)
 
-    dice_roll = roll_format.format(num_dice=num_dice, num_dice_sides=num_dice_sides, adv_dis=adv_dis, modifier=modifier)
+    dice_roll = roll_format.format(num_dice=num_dice, num_dice_sides=num_dice_sides, adv_dis=adv_dis,
+                                   modifier=modifier)
 
     return dice_roll
 
 
 def generate_roll_string(command, modifier):
     return calculate_dice_roll(command.num_dice, command.num_dice_sides, command.modifier,
-                        modifier.extra_modifier.get(), modifier.advantage.get(), modifier.disadvantage.get(), modifier.critical_hit.get())
+                               modifier.extra_modifier.get(), modifier.advantage.get(),
+                               modifier.disadvantage.get(), modifier.critical_hit.get())
--- dice_maiden_ui/configuration.py
+++ dice_maiden_ui/configuration.py
@@ -13,4 +13,3 @@ def validate_config_against_schema(config_json):
     schema_json = open_json_file('../data/configuration.schema')
 
     jsonschema.validate(config_json, schema_json)
-
